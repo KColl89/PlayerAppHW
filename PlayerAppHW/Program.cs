0de3e8e46ab2499688288162b4a46b93638f4d2f
Kendrick C <yung_thoed@yahoo.com>
2022-05-24 04:09:08 -0500

    Step two: Prompt the user for the number of players to be created.

--- PlayerAppHW/Program.cs
+++ PlayerAppHW/Program.cs
@@ -10,6 +10,13 @@ namespace PlayerAppHW
     {
         public static void Main(string[] args)
         {
+            int amount = 0;
+            
+            //Prompts the user for the number of players to be created.
+            Console.WriteLine("How many players would you like to create? :");
+            amount = Convert.ToInt32(Console.ReadLine());
+
+           
             Player p1 = new();
             p1.Name = " Kendrick";
             p1.Email = "KDot@gmail.com";
