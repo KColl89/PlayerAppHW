488440d5ac85b23afd66dcaae8c4b4b8495c54d5
Kendrick C <yung_thoed@yahoo.com>
2022-05-24 05:35:26 -0500

    Step three: Initialize player model and populate it with information.

--- PlayerAppHW/Program.cs
+++ PlayerAppHW/Program.cs
@@ -11,17 +11,37 @@ namespace PlayerAppHW
         public static void Main(string[] args)
         {
             int amount = 0;
+           
             
             //Prompts the user for the number of players to be created.
             Console.WriteLine("How many players would you like to create? :");
             amount = Convert.ToInt32(Console.ReadLine());
 
-           
+
+            Player[] player = new Player[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                Console.WriteLine("Enter player name and email:");
+                player[i] = new Player();
+                player[i].Name = Console.ReadLine();
+                player[i].Email = Console.ReadLine();
+                
+            }
+
+           for (int i = 0; i < amount; i++)
+            {
+                Console.WriteLine($"Player name: {player[i].Name} id: {player[i].ID} email: {player[i].Email}");
+            }
+
+            /*
             Player p1 = new();
             p1.Name = " Kendrick";
             p1.Email = "KDot@gmail.com";
-
+                 
             Console.WriteLine($"Player name: {p1.Name} id: {p1.ID} email {p1.Email}");
+            */
+
+
         }
     }
 }
